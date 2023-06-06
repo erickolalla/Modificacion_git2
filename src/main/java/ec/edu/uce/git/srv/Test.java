d4229266c3473477ac0dffeb80af2a646be58116
erickolalla <erickfabri_01@hotmail.com>
2023-06-06 00:02:20 -0500

    Versión 1.2

--- src/main/java/ec/edu/uce/git/srv/Test.java
+++ src/main/java/ec/edu/uce/git/srv/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public void saludo() {
         System.out.println("Hola Mundo, mi primer git clone");
         System.out.println("Mensaje de modificación de versionamiento");
+        System.out.println("Version 1.2");
     }
 }
